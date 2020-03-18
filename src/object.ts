3febd2c5bc66fbfbeba585b1f323ff713a1a043c
James Marks <jmarks@landlordwebsolutions.com>
2020-03-18 10:54:14 -0400

    Added `prop()` to substitute `take().from()`

--- src/object.ts
+++ src/object.ts
@@ -233,3 +233,11 @@ export const iterateWithBreak = (
     return { done };
   }, { done: false });
 };
+
+/**
+ * Gets a value from an object property with an optional default value.
+ * @param o
+ * @param key
+ * @param defaultValue
+ */
+export const prop = (o: Hash, key: string, defaultValue: any = null): any => (hasKey(o, key) ? o[key] : defaultValue)
