b572de0114c6715de428454e146d899f6de455a6
James Marks <jamesgmarks@gmail.com>
2020-03-23 08:11:38 -0400

    Missed one

--- src/miscellaneous.ts
+++ src/miscellaneous.ts
@@ -1,4 +1,4 @@
-import { isObject } from './object';
+import { isDefinedObject } from './object';
 import { isArray, count } from './array';
 import { keys } from '.';
 
@@ -13,11 +13,11 @@ export const isNull = (x: any) => x === null;
 export const notNull = (x: any) => !isNull(x);
 export const empty = (x: any) => (
   undef(x) || isNull(x) || x === 0 || x === '' || x === '0' || x === false
-  || (isArray(x) && x.length === 0) || (isObject(x) && count(keys(x)) === 0)
+  || (isArray(x) && x.length === 0) || (isDefinedObject(x) && count(keys(x)) === 0)
 );
 export const notEmpty = (x: any) => !empty(x);
 
 export const boolVal = (x: any): boolean => !!x;
 export const floatVal = (x: any): number => parseFloat(x);
 export const intVal = (x: any): number => parseInt(x, 10);
-export const strVal = (x: any): string => ((isObject(x) || isArray(x)) ? JSON.stringify(x) : `${x}`);
+export const strVal = (x: any): string => ((isDefinedObject(x) || isArray(x)) ? JSON.stringify(x) : `${x}`);
