e25faa6bbfe823e786960a0a78e30970256a269a
James Marks <jmarks@landlordwebsolutions.com>
2020-03-30 15:26:38 -0400

    Added `isNumeric`

--- src/miscellaneous.ts
+++ src/miscellaneous.ts
@@ -17,8 +17,13 @@ export type RequireOnlyOne<T, Keys extends keyof T = keyof T> =
       [K in Keys]-?: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, undefined>>
   }[Keys]
 
+/** Returns true if `x` is a `number`. NaN is considered a number and so will return `true`. Does not return true for `BigInt`. */
 export const isNumber = (x: any) => (typeof x === 'number');
+/** Returns true if `x` is a `number` that is not NaN. Does not return true for `BigInt`.  */
 export const isValidNumber = (x: any) => isNumber(x) && !Number.isNaN(x);
+/** Returns true if `x` is a `number` or a string that can be successfully parsed to a `number` that is not `NaN` */
+export const isNumeric = (x: any) => isValidNumber(parseInt(`${x}`, 10));
+
 export const isBigInt = (x: any) => (typeof x === 'bigint');
 export const isString = (x: any) => (typeof x === 'string');
 export const isSymbol = (x: any) => (typeof x === 'symbol');
