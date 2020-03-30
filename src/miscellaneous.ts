1279db0bed26c9334a2f6e6127be5b69a3ae84e6
James Marks <jmarks@landlordwebsolutions.com>
2020-03-30 15:29:03 -0400

    Fix: Should have used Float

--- src/miscellaneous.ts
+++ src/miscellaneous.ts
@@ -22,7 +22,7 @@ export const isNumber = (x: any) => (typeof x === 'number');
 /** Returns true if `x` is a `number` that is not NaN. Does not return true for `BigInt`.  */
 export const isValidNumber = (x: any) => isNumber(x) && !Number.isNaN(x);
 /** Returns true if `x` is a `number` or a string that can be successfully parsed to a `number` that is not `NaN` */
-export const isNumeric = (x: any) => isValidNumber(parseInt(`${x}`, 10));
+export const isNumeric = (x: any) => isValidNumber(Number.parseFloat(`${x}`));
 
 export const isBigInt = (x: any) => (typeof x === 'bigint');
 export const isString = (x: any) => (typeof x === 'string');
