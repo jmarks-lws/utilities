87e1b14d9f78a6c9e6d850b5ebc4e934b92edcd3
James Marks <jamesgmarks@gmail.com>
2020-04-06 11:10:32 -0400

    strVal will now return empty string for undefined and null input.

--- src/miscellaneous.ts
+++ src/miscellaneous.ts
@@ -46,6 +46,6 @@ export const empty = (x: any) => (
 export const notEmpty = (x: any) => !empty(x);
 
 export const boolVal = (x: any): boolean => !!x;
-export const floatVal = (x: any): number => parseFloat(x);
+export const floatVal = (x: any): number => ((typeof x === 'number') ? x : parseFloat(x));
 export const intVal = (x: any): number => parseInt(x, 10);
-export const strVal = (x: any): string => ((isDefinedObject(x) || isArray(x)) ? JSON.stringify(x) : `${x}`);
+export const strVal = (x: any): string => ((isDefinedObject(x) || isArray(x)) ? JSON.stringify(x) : `${x || ''}`);
