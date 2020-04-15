dfe586cc0f584a5ed7154680c294614c92593ff3
James Marks <jamesgmarks@gmail.com>
2020-04-15 09:28:32 -0400

    Better typing / type hinting for

--- src/object.ts
+++ src/object.ts
@@ -288,10 +288,10 @@ export const arraysToObject = (
  * Works similarly to mapping an array, but maps the values of `o`, returning a new object that has
  * the same keys with values each transformed by `fn`.
  */
-export const transformValues = (o: Hash, fn: (value: any) => any) => reduce(
+export const transformValues = <T, U>(o: HashOf<T>, fn: (value: T) => U) : HashOf<U> => reduce(
   keys(o),
-  (acc: Hash, key) => ({ ...acc, [key]: fn(o[key]) }),
-  {} as Hash,
+  (acc: HashOf<U>, key) => ({ ...acc, [key]: fn(o[key]) }),
+  {} as HashOf<U>,
 )
 
 /**
