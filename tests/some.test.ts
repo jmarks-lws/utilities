59e722d170711d73a713d756afedc54f01e1ed07
James Marks <jamesgmarks@gmail.com>
2020-04-28 09:45:27 -0400

    Removed unused test

--- tests/some.test.ts
+++
@@ -1,8 +0,0 @@
-import { chopFirst, chopLast, where } from '../src/array'
-
-describe('quicker tests', () => {
-  test('where', async () => {
-    const result = where([1, 2, 3, 4, 5, 6], (a) => a < 3);
-    expect(result).toMatchObject([1, 2])
-  })
-})
