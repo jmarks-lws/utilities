f2d468d44734e615dd1bc96cbf2f79cb28292fe4
James Marks <jamesgmarks@gmail.com>
2020-06-30 11:10:46 -0400

    Fixed type error

--- src/objects/transformKeys.ts
+++ src/objects/transformKeys.ts
@@ -6,7 +6,7 @@ import { reduce } from '../arrays/reduce';
  * Works similarly to mapping an array, but maps the keys of `o`, returning a new object that has
  * the same values with their keys each transformed by `fn`.
  */
-export const transformKeys = <T>(o: HashOf<T>, fn: (key: string) => string) : HashOf<T> => reduce(
+export const transformKeys = <T>(o: HashOf<T>, fn: (key: string | number) => string) : HashOf<T> => reduce(
   keys(o),
   (acc: HashOf<T>, key) => ({ ...acc, [fn(key)]: o[key] }),
   {} as HashOf<T>,
