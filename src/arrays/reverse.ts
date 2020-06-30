1e6113af04821b35f94db407cc4c2c577c658e47
James Marks <jamesgmarks@gmail.com>
2020-06-30 11:07:47 -0400

    Updated `reverse()`

--- src/arrays/reverse.ts
+++ src/arrays/reverse.ts
@@ -3,6 +3,6 @@ import { arrayCopy } from './arrayCopy';
 /**
  * Returns a new array that is the result of reversing the order of the elements in `array`
  */
-export const reverse = (
-  array: any[],
-): any[] => (arrayCopy(array)?.reverse()!);
+export const reverse = <T>(
+  array: T[],
+): T[] => (arrayCopy(array)!.reverse());
