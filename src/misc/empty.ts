f04ba2759c01e0372f39d322671ef6b5888465c9
James Marks <jmarks@landlordwebsolutions.com>
2020-07-21 09:42:26 -0400

    Improved docs for empty()

--- src/misc/empty.ts
+++ src/misc/empty.ts
@@ -8,6 +8,7 @@ import { keys } from '../objects/keys';
 /**
  * Closely equivalent to PHP's rules for empty().
  *  - Note: Treats both `undefined` and `null` as PHP's `null` for this method.
+ * Returns true if x is undefined, null, 0, '' (empty string), '0', false, an empty array or an object with no keys.
  * */
 export const empty = (x: any): boolean => (
   undef(x) || isNull(x) || x === 0 || x === '' || x === '0' || x === false
