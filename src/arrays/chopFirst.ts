587bb66c429834bc457e962fa15eeac13b056888
James Marks <jamesgmarks@gmail.com>
2021-06-06 10:40:07 -0400

    Fixed number entry for chopFirst

--- src/arrays/chopFirst.ts
+++ src/arrays/chopFirst.ts
@@ -1,9 +1,7 @@
-import { slice } from './slice';
-import { arrayWrap } from './arrayWrap';
 /**
  * Retreives the first `n` elements from `array` and returns as a new Array.
  */
-export const chopFirst = <T>(list: T[]): T[] => {
+export const chopFirst = <T>(list: T[], n: number = 1): T[] => {
   const [ first, ...rest ] = list;
-  return rest;
+  return n > 1 ? chopFirst(rest, n - 1) : rest;
 };
