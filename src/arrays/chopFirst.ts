97ad507e07f13099d8326dd6b1bd653f1c7d48ed
James Marks <jmarks@rentsync.com>
2021-08-28 09:34:05 -0400

    Fix broken chopFirst

--- src/arrays/chopFirst.ts
+++ src/arrays/chopFirst.ts
@@ -1,7 +1,7 @@
+import { arrayWrap } from './arrayWrap';
+import { slice } from './slice';
+
 /**
  * Retreives the first `n` elements from `array` and returns as a new Array.
  */
-export const chopFirst = <T>(list: T[], n: number = 1): T[] => {
-  const [ first, ...rest ] = list;
-  return n > 1 ? chopFirst(rest, n - 1) : rest;
-};
+export const chopFirst = (array: any[], n: number = 1): any => slice(arrayWrap(array), 0, n);
