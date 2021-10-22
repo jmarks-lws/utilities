6c4d5b35d8fb8c548ef493c3a6d1b6842ca7bb70
James Marks <jmarks@rentsync.com>
2021-10-22 07:55:22 -0400

    revert typings for remapKeys

--- src/objects/remapKeys.ts
+++ src/objects/remapKeys.ts
@@ -3,7 +3,6 @@ import { Hash } from './types/Hash';
 import { hasKey } from './hasKey';
 import { merge } from './merge';
 import { keys } from './keys';
-import { HashOf } from './types/HashOf';
 
 /**
  * Returns a new object derived from `obj` where the keys are changed as described by `remap`, optionally including or omitting remaining data.
@@ -13,21 +12,10 @@ import { HashOf } from './types/HashOf';
  * @param returnAll Default `false`. If this is true, all data from the source object will be returned with only the key names in
  *                  `remap` being changed. All other keys and values will remain as they were in the source object.
  */
-export const remapKeys = <
-  TVal extends unknown,
-  TIn extends HashOf<TVal>,
-  TMap extends { [key in keyof Partial<TIn>]: string },
-  TReturn extends { [key in keyof Partial<TIn>]: TVal }
->(
-    obj: TIn,
-    remap: TMap,
-    returnAll: boolean = false,
-  ): TReturn => (
-    reduce(
-      keys(obj) as string[],
-      (acc: Partial<TReturn>, k: keyof TIn) => merge(
-        { ...acc },
-        ((hasKey(remap, k) || returnAll) ? { [(hasKey(remap, k) ? remap[k] : k)]: obj[k] } : {}) as Partial<TReturn>,
-      ), {} as Partial<TReturn>,
-    ) as TReturn
-  );
+export const remapKeys = (obj: Hash, remap: Hash, returnAll: boolean = false): Hash => reduce(
+  keys(obj) as string[],
+  (acc: Hash, k: string) => merge(
+    { ...acc },
+    ((hasKey(remap, k) || returnAll) ? { [(hasKey(remap, k) ? remap[k] : k)]: obj[k] } : {}),
+  ), {} as Hash,
+);
