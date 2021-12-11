89e70e62a0197e57efad7c5b62327ad38c4d7fbc
James Marks <jmarks@rentsync.com>
2021-12-11 13:52:23 -0500

    further typing improvements for isArray

--- src/arrays/isArray.ts
+++ src/arrays/isArray.ts
@@ -1,3 +1,3 @@
 
 /** Wrapper for native `Array.isArray()` */
-export const isArray = <T>(x: T | T[]): x is T[] => Array.isArray(x);
+export const isArray = <T>(x: T[] | unknown): x is T[] => Array.isArray(x);
