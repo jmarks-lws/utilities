2ccf178968a9b8faacf133bfd877f3a6ca2c213f
Brendan Dagys <brendandagys@gmail.com>
2022-08-16 20:57:21 -0400

    Improve JSDoc consistency with rest of library

--- src/misc/stringToNumber.ts
+++ src/misc/stringToNumber.ts
@@ -1,9 +1,8 @@
 /**
- * Returns a numeric representation of an input string.
- * The string represents one or more digits, with exactly one
- * optional decimal place, at any location in the string.
- * This may return NaN.
- * @param str String that represents a number, optionally with one decimal place.
+ * Returns a numeric representation of an input string or `NaN` if the provided string value is not valid.
+ * The string should consist one or more digits, with zero or one decimal point (period) within the string.
+ * If the string does not match this pattern, this function will return `NaN`.
+ * @param str String that should represent a number, with one optional decimal point.
  */
 export const stringToNumber = (str: string): number => (
   /^((\d+(\.\d*)?)|\.\d+)$/.test(str) ? parseFloat(str) : NaN
