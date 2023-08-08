ccf922e9ceea793e734aa42b4121fa13d8945b66
uzher-code <uazhar.git@gmail.com>
2023-08-07 23:28:36 -0400

    fixed jsdoc

--- src/arrays/duplicateCountDictionary.ts
+++ src/arrays/duplicateCountDictionary.ts
@@ -4,7 +4,7 @@ import { TCountDictionary } from 'src/miscellaneous';
 /**
  * Helper method: creates a dictionary where the key is the value of array item
  * and the values are the number of times it shows up along  as a TCountDictionary<T>
- * @param array - array to get the duplicate count
+ * @param array - array of type T
  * @returns a hash of TCountDictionary<T>, where value is the the number of times an
  * array item appears along with the item
  * e.g. for array [1,1,2,3] => {'1': {count: 2, value: 1}, '2': {count: 1, value: 2}, '3': {count: 1, value: 3}}
